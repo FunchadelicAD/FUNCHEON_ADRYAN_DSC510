58bf3c40b9a4ae6ace6679f961975981e625541b
Funchadelic <adryanf@gmail>
2019-05-09 16:53:22 -0500

    working on def's

--- Learning/wk8 - Assignment.py
+++ Learning/wk8 - Assignment.py
@@ -1,26 +1,34 @@
 import string
-gba_file = open('gettysburg.txt', 'r') #pulling in txt file
-counts = dict() #creating the dictionary
-for line in gba_file: #process_lines
-    line = line.translate(str.maketrans('', '', string.punctuation)) #removing spaces and puntuation
-    line = line.lower() #converting to lowercase
-    words = line.split()
-    for word in words:  #add_words
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] += 1
+gba_file = open('gettysburg.txt', 'r')  # pulling in txt file
+counts = dict()  # creating the dictionary
+
+def process_line(gba_file):
+    for line in gba_file:
+        line = line.translate(str.maketrans('', '', string.punctuation))
+        line = line.lower()
+        words = line.split()
+        for word in words:
+            if word not in counts:
+                counts[word] = 1
+            else:
+                counts[word] += 1
 
 # Sort the dictionary by value
-lst = list()
-for key, val in list(counts.items()):
-    lst.append((val, key))
-
-def print_pretty():
+def add_word(list):
+    lst = list()
+    for key, val in list(counts.items()):
+        lst.append((val, key))
+def print_pretty(lst):
     lst.sort(reverse=True)
     print('Length of dictionary:',sum(counts.values()))
     print('Count', 'Word')
 
     for key, val in lst[:]:
         print(key, val)
-    return print_pretty()
+
+def main():
+    process_line()
+    add_word()
+
+
+
